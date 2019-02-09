8a6d051a1445e393526e9960331cc7c4af088183
connernance1 <39962949+connernance1@users.noreply.github.com>
2019-02-08 23:27:21 -0700

    working on ^ # method.

--- src/Shell.java
+++ src/Shell.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
@@ -14,12 +16,29 @@ public class Shell {
         String fP = System.getProperty("user.dir");
         File filePath = new File(fP);
         boolean execute = true;
+        List<String> history = new ArrayList<String>();
+        int ct = 1;
 
         String[] input = args;
-
+        System.out.println("\n" +
+                " _______                           _________.__           .__  .__   \n" +
+                " \\      \\ _____    ____   ____    /   _____/|  |__   ____ |  | |  |  \n" +
+                " /   |   \\\\__  \\  /    \\ /  _ \\   \\_____  \\ |  |  \\_/ __ \\|  | |  |  \n" +
+                "/    |    \\/ __ \\|   |  (  <_> )  /        \\|   Y  \\  ___/|  |_|  |__\n" +
+                "\\____|__  (____  /___|  /\\____/  /_______  /|___|  /\\___  >____/____/\n" +
+                "        \\/     \\/     \\/                 \\/      \\/     \\/           \n" +
+                "                                                                     \n" +
+                "                                                                     \n" +
+                "                                                                     \n" +
+                "                                                                     \n" +
+                "                                                                     \n" +
+                "                                                                     \n");
         while(execute){
-            System.out.print("[" + filePath + "]" + ": ");
-            String Arg = scanner.next();
+
+
+            System.out.print("[" + filePath + "]" + " : " + System.getProperty("user.name") + "@Desktop" + " $ ");
+            String Arg = scanner.nextLine();
+            System.out.println("\n");
 
             if(Arg.equals("exit")){
                 execute = false;
@@ -27,40 +46,75 @@ public class Shell {
 
             try {
                 String[] validCommand = {"ls", "-l", "cd..", "ptime", "cd", "exit"};
-                splitCommand(Arg);
+                String[] A = splitCommand(Arg);
 
-                if(Arg.equals("ls")){
+//                for(String item : A){
+//                    System.out.println(item + "\n");
+//                }
+
+
+                if(A[0].equals("ls")){
                     File currentDir = new File(System.getProperty("user.dir"));
                     File[] fList = currentDir.listFiles();
-                    System.out.println("\n");
-                    for(File f : fList){
-                        System.out.println(f.getName());
 
+                    if(A.length > 1){
+                        if(A[1].equals("-l")){
+                            System.out.println("Total: " + fList.length);
+                        }
+                        history.add(ct + " : " + "ls -l");
+                        ct++;
+                        System.out.println("\n");
                     }
-
+                    else if(A.length == 1){
+                        System.out.println("\n");
+                        for(File f : fList){
+                            System.out.println(f.getName());
+                        }
+                        history.add(ct + " : " + "ls");
+                        ct++;
+                        System.out.println("\n");
+                    }
+                    else {
+                        System.out.println("Please enter a valid Argument: " + A[1] + "\n");
+                    }
                 }
-                else if(Arg.equals("cd..")){
+                else if(A[0].equals("cd..")){
                     File currentDir = new File(System.getProperty("user.dir"));
                     String f = currentDir.getParent();
                     System.setProperty("user.dir", f);
                     File newDir = new File(f);
-
                     filePath = newDir;
 
+                    history.add(ct + " : " + "cd.. ");
+                    ct++;
                 }
-                else if (Arg.equals("cd")){
+                else if (A[0].equals("cd")){
                     File currentDir = new File(System.getProperty("user.dir"));
                     File[] fList = currentDir.listFiles();
 
-                    for(int i = 0; i < fList.length; i++){
-                        if(fList[i].equals(input[1])){
-                            String f = System.getProperty("user.dir");
-                            System.setProperty("user.dir", f);
-                            String dirF = f + "/" + fList[i];
+                    for(File item : fList){
+
+                        if(item.getName().equals(A[1])){
+                            String dirF = "/" + item;
+                            System.setProperty("user.dir", dirF);
                             File newDir = new File(dirF);
                             filePath = newDir;
                         }
                     }
+                    history.add(ct + " : " + "cd " + A[1]);
+                    ct++;
+                }
+                else if(A[0].equals("history")){
+                    System.out.println("-- Command History --");
+                    for(String item : history){
+                        System.out.println(item);
+                    }
+                    history.add(ct + " : " + "history");
+                    ct++;
+                }
+                else if(A[0].equals("^")){
+
+
                 }
                 else {
                     System.out.println("Please Enter a valid Command. B*tch.");
